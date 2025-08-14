b90fdad20c301ad4649fb7be0351ca58296043a7
ifubaraboye <ifubaraboyeoribime@outlook.com>
2025-08-14 12:57:43 +0100

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter, JetBrains_Mono } from "next/font/google"
 import "./globals.css"
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   variable: "--font-inter",
@@ -84,6 +85,7 @@ export default function RootLayout({
               </div>
             </div>
           </nav>
+          
 
           {/* Main Content */}
           <main className="max-w-7xl mx-auto px-6 lg:px-8 py-12">{children}</main>
@@ -97,6 +99,7 @@ export default function RootLayout({
             </div>
           </footer>
         </div>
+        <Analytics />
       </body>
     </html>
   )
